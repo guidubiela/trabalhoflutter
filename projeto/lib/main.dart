26057f53855ae7710a8b3239a1b164ee24f995d0
Guilherme <guidubiela@gmail.com>
2023-07-06 22:30:29 -0300

    feito

--- projeto/lib/main.dart
+++ projeto/lib/main.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Padaria());
 }
 
-class MyApp extends StatelessWidget {
+class Product {
+  final String nome;
+  final double preco;
+  int quantidade;
+
+  Product({required this.nome, required this.preco, this.quantidade = 0});
+}
+
+class Padaria extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Padaria App',
       theme: ThemeData(
-        primarySwatch: Color.fromARGB(255, 182, 157, 87),
+        primarySwatch: Colors.amber,
       ),
       home: ProductListScreen(),
     );
   }
-}
-
-class Product {
-  final String name;
-  final double price;
-  bool selected;
-
-  Product({required this.name, required this.price, this.selected = false});
 }
 
 class ProductListScreen extends StatefulWidget {
@@ -32,30 +32,40 @@ class ProductListScreen extends StatefulWidget {
 
 class _ProductListScreenState extends State<ProductListScreen> {
   List<Product> products = [
-    Product(name: "Pão", price: 1.50),
-    Product(name: "Bolo", price: 15.00),
-    Product(name: "Croissant", price: 2.00),
-    Product(name: "Rosquinha", price: 0.50),
+    Product(nome: "Pão", preco: 1.50),
+    Product(nome: "Bolo", preco: 15.00),
+    Product(nome: "Torta", preco: 20.00),
+    Product(nome: "Rosca", preco: 8.50),
+    Product(nome: "Brigadeiro", preco: 1.50),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Produtos de Padaria'),
+        title: Text('Produtos da Padoca'),
       ),
       body: ListView.builder(
         itemCount: products.length,
         itemBuilder: (context, index) {
-          return CheckboxListTile(
-            title: Text(products[index].name),
-            subtitle: Text('R\$ ${products[index].price.toStringAsFixed(2)}'),
-            value: products[index].selected,
-            onChanged: (value) {
-              setState(() {
-                products[index].selected = value!;
-              });
-            },
+          return ListTile(
+            title: Text(products[index].nome),
+            subtitle: Text('R\$ ${products[index].preco.toStringAsFixed(2)}'),
+            trailing: DropdownButton<int>(
+              value: products[index].quantidade,
+              onChanged: (value) {
+                setState(() {
+                  products[index].quantidade = value!;
+                });
+              },
+              items: List.generate(10, (index) => index)
+                  .map<DropdownMenuItem<int>>((int value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text(value.toString()),
+                );
+              }).toList(),
+            ),
           );
         },
       ),
@@ -63,13 +73,12 @@ class _ProductListScreenState extends State<ProductListScreen> {
         child: Icon(Icons.arrow_forward),
         onPressed: () {
           List<Product> selectedProducts =
-              products.where((product) => product.selected).toList();
+              products.where((product) => product.quantidade > 0).toList();
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => SelectedProductsScreen(
-                selectedProducts: selectedProducts,
-              ),
+              builder: (context) =>
+                  SelectedProductsScreen(selectedProducts: selectedProducts),
             ),
           );
         },
@@ -83,10 +92,10 @@ class SelectedProductsScreen extends StatelessWidget {
 
   SelectedProductsScreen({required this.selectedProducts});
 
-  double calculateTotalPrice() {
+  double calcularTotalPreco() {
     double total = 0.0;
     for (var product in selectedProducts) {
-      total += product.price;
+      total += product.preco * product.quantidade;
     }
     return total;
   }
@@ -104,9 +113,12 @@ class SelectedProductsScreen extends StatelessWidget {
               itemCount: selectedProducts.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(selectedProducts[index].name),
+                  title: Text(selectedProducts[index].nome),
                   subtitle: Text(
-                      'R\$ ${selectedProducts[index].price.toStringAsFixed(2)}'),
+                      'R\$ ${selectedProducts[index].preco.toStringAsFixed(2)}'),
+                  trailing: Text(
+                    'Quantidade: ${selectedProducts[index].quantidade}',
+                  ),
                 );
               },
             ),
@@ -114,11 +126,11 @@ class SelectedProductsScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Text(
-              'Total: R\$ ${calculateTotalPrice().toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              'Total: R\$ ${calcularTotalPreco().toStringAsFixed(2)}',
+              style: TextStyle(fontSize:18, fontWeight: FontWeight.bold),
             ),
           ),
-        ],
+        ], 
       ),
     );
   }
